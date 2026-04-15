4bcbac1e2ce31dbb120a09013da408a918ca496e
phung anh van <phunganhvan13122004@gmail.com>
2026-04-15 11:33:47 +0700

    test dark mode with light mode for navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,30 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { useSelector } from 'react-redux/es/hooks/useSelector';
+import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 const Header = () => {
-  const users= useSelector((state: any) => state.user.listUsers);
-    return (
-        <>
-        <Navbar className="bg-body-tertiary">
+  const users = useSelector((state: any) => state.user.listUsers);
+  const [mode, setMode] = useState("light");
+
+  return (
+    <>
+      <Navbar className="bg-body-tertiary" data-bs-theme={mode}>
         <Container>
           <Navbar.Brand href="#home">Navbar with text {users.length}</Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
-              Signed in as: <a href="#login">Mark Otto</a>
-            </Navbar.Text>
+            <Form.Check
+              value={mode}
+              onChange= {(e) => setMode (e.target.value === "light" ? "dark" : "light")}
+              type="switch"
+              id="custom-switch"
+              label={mode === "light" ? <Navbar.Text>Light Mode</Navbar.Text> : <Navbar.Text>Dark Mode</Navbar.Text>}
+            />
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      </>
-    );
+    </>
+  );
 }
 export default Header;
